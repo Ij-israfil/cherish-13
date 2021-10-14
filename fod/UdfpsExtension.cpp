20f2e1a9b38933a35dedf13de4d484a4ebb2d7eb
ThatisJigen <gianfranco.liguori.96@gmail.com>
2021-10-14 19:08:01 +0200

    RMX1901: FOD --> UDFPS
    keep the 2.1 wrapper and implement only 2.3 functions *TODO: handle AOD state *remove vintf manifest for 2.1 istance

--- fod/UdfpsExtension.cpp
+++ fod/UdfpsExtension.cpp
@@ -1,11 +1,11 @@
 /*
- * Copyright (C) 2020 Harshit Jain <god@hyper-labs.tech>
+ * Copyright (C) 2020 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,23 +14,6 @@
  * limitations under the License.
  */
 
-<<<<<<< HEAD:RealmeProximityService/Android.bp
-android_app {
-    name: "RealmeProximityHelper",
-
-    srcs: ["src/**/*.java"],
-
-    manifest: "AndroidManifest.xml",
-
-    platform_apis: true,
-    certificate: "platform",
-    privileged: true,
-
-    optimize: {
-        proguard_flags_files: ["proguard.flags"],
-    },
-}
-=======
 #include <compositionengine/UdfpsExtension.h>
 
 uint32_t getUdfpsZOrder(uint32_t z, bool touched) {
@@ -40,4 +23,3 @@ uint32_t getUdfpsZOrder(uint32_t z, bool touched) {
 uint64_t getUdfpsUsageBits(uint64_t usageBits, bool) {
     return usageBits;
 }
->>>>>>> 3ee6cdb... RMX1921: fod->udfps:RealmeProximityService/UdfpsExtension.cpp
