6fff2e9239827961bf394868632d40f19486d514
ThatisJigen <gianfranco.liguori.96@gmail.com>
2021-11-03 18:37:41 +0100

    RMX1901: delete data/system/package_cache after decrypt
    fixes dirty flash resources mess

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -3021,6 +3021,9 @@ case "$target" in
       echo "0:1209600 1:0 2:0 3:0 4:0 5:0 6:2054400 7:0" > /sys/module/cpu_boost/parameters/powerkey_input_boost_freq
       echo 400 > /sys/module/cpu_boost/parameters/powerkey_input_boost_ms
 
+      # Remove settings cache, avoids derps after dirty flash
+      rm -rf /data/system/package_cache
+
       # Set Memory parameters
       configure_memory_parameters
 
