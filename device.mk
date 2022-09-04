cbd70a92a7637063ddd38719c0b91fc0514cf0f9
luk1337 <priv.luk@gmail.com>
2022-09-04 11:25:15 +0000

    RMX1901: Import sensors/2.0/multihal

--- device.mk
+++ device.mk
@@ -423,7 +423,7 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.0-service.multihal \
+    android.hardware.sensors@2.0-service.multihal.xt \
     libsensorndkbridge
 
 # Soong namespaces
